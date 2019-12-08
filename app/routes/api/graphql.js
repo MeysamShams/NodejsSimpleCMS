5ccfa90bff56a21081daa8291a796e45642edbb0
meysam <meysam.shm@outlook.com>
2019-12-08 10:42:58 +0330

    create graphql Queries

--- app/routes/api/graphql.js
+++ app/routes/api/graphql.js
@@ -2,7 +2,52 @@ const {gql}=require('apollo-server-express');
 
 let typeDefs=gql`
     type Query{
-        user:String
+        sliders:[Slider],
+        categories:[Category],
+        posts:[Post],
+        post(id:String):Post,
+        user(id:String):User,
+        comments(postId:String):[Comment]
+    }
+
+    type Slider{
+        image:String,
+        href:String
+    }
+    type Category{
+        id:String,
+        image:String,
+        name:String
+    }
+    type Post{
+        id:String,
+        author:User,
+        file:File,
+        category:Category
+        title:String,
+        body:String,
+        image:String,
+        tags:String,
+        commentsCount:String,
+        date:String
+    }
+    type User{
+        id:String,
+        name:String,
+        username:String,
+        avatar:String,
+    }
+    type Comment{
+        id:String,
+        author:User,
+        parent:Comment,
+        body:String,
+        date:String
+    }
+    type File{
+        name:String,
+        link:String,
+        size:String
     }
 `;
 
